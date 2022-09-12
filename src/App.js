9a044e52fed4ddb6034d1950004b832ca8c3753e
Ana Carolina dos Santos Rocha <78958795+annakks@users.noreply.github.com>
2022-09-12 00:59:51 -0300

    feat: Create header with responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
+import Header from './Component/Header/Header'
+import './App.css'
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
+  return <Header />
 }
 
 export default App
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import "./Header.css"
+import logo from '../../assets/logo-removebg-preview.png'
+
+const Header = () => {
+    // navbar fixed
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector(".header")
+        header.classList.toggle("active", window.scrollY > 100)
+    })
+    // navbar mobile
+    const [Mobile, setMobile] = useState(false)
+    return (
+        <>
+        <header className="header">
+            <div className="container d_flex">
+                <div className="logo">
+                    <img src={logo}  alt="" width='100px' />
+                </div>
+                <div className="navlink">
+                    <ul className= {Mobile ? "nav-links-mobile" : "link f_flex uppercase" } onClick={() => setMobile(false)}>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">Sobre mim</a></li>
+                        <li><a href="#portifolio">Portfólio</a></li>
+                        <li><a href="#softskill">SoftSkills</a></li>
+                        <li><a href="#hardskill">HardSkills</a></li>
+                        <li><a href="#contact">Contato</a></li>
+                    </ul>
+                    <button className='toggle' onClick={() => setMobile(!Mobile)}>
+                        {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
+                    </button>
+                </div>
+            </div>
+        </header>
+        </>
+    )
+}
+
+export default Header
